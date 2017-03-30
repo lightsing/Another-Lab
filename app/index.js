13815630f82065f44ce7e36a7c99caa9494a17bd
lightsing <light.tsing@gmail.com>
2017-03-30 19:57:01 +0800

    Fix login bugs

--- app/index.js
+++ app/index.js
@@ -4,8 +4,8 @@ const PEST = require('./libs/PEST/PESTest-client');
 
 async function login(info) {
   var client = new PEST.PESTClient(info);
-  console.log(info);
   var res = await client.login();
+  await client.logout(); // login is only use for validate the password.
   console.log(res);
-  return res;
+  return client;
 }
