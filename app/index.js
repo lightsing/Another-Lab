061de8ec72b95fc20c4b8728a81e0caf7e13354c
lightsing <light.tsing@gmail.com>
2017-03-30 18:36:20 +0800

    Change to jQuery

--- app/index.js
+++ app/index.js
@@ -1,12 +1,7 @@
 "use strict";
 
-const lib = require('./libs/PEST/PESTest-client');
+const PEST = require('./libs/PEST/PESTest-client');
 
-function login(e) {
-    if (e.preventDefault) e.preventDefault();
-    const form = e.srcElement.childNodes[1].childNodes[3];
-    const info = { username: form.childNodes[1].childNodes[1].value,
-                   password: form.childNodes[3].childNodes[1].value };
-    console.log(username+'\n'+password);
-    return false;
+function login(info) {
+    console.log(JSON.stringify(info));
 }
