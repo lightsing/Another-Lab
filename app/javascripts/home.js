0d7f5feeca7d62b3db4de7ed02d2fa8ddd9efb1f
lightsing <light.tsing@gmail.com>
2017-03-31 13:28:17 +0800

    Add homepage

---
+++ app/javascripts/home.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const PEST = require('./libs/PEST/PESTest-client');
+
+function getParameterByName(name, url) {
+  if (!url) {
+    url = window.location.href;
+  }
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getInfo() {
+  var userID = getParameterByName('userID');
+  var password = getParameterByName('password');
+  if( userID && password ) {
+    return { userID: userID, password:password };
+  }
+}
+
+function newClient() {
+  var info = getInfo();
+  if( info ) {
+    return new PEST.PESTClient(info);
+  }
+}
