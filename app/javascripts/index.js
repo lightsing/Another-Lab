a3236f3ca467bb1a92f59d988d0003fd1367e79a
lightsing <light.tsing@gmail.com>
2017-03-31 13:39:27 +0800

    Update Color

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -6,6 +6,5 @@ async function login(info) {
   var client = new PEST.PESTClient(info);
   var res = await client.login();
   await client.logout(); // login is only use for validate the password.
-  console.log(res);
   return client;
 }
